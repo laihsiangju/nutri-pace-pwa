390e4ea5a003084aa04548570aae4aee42c68e9b
laihsiangju <248847442+laihsiangju@users.noreply.github.com>
2026-07-02 17:45:37 +0800

    Configure Google OAuth client

--- app-config.js
+++ app-config.js
@@ -1,4 +1,4 @@
 window.NUTRI_PACE_CONFIG = {
-  googleClientId: "PASTE_GOOGLE_OAUTH_CLIENT_ID_HERE",
+  googleClientId: "473230349726-2ggli79mkg8sb8fp99g67qkdakdi8b4k.apps.googleusercontent.com",
   driveFileName: "nutri-pace-data.json"
 };
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nutri-pace-pwa-v8";
+const CACHE_NAME = "nutri-pace-pwa-v9";
 const APP_SHELL = [
   "./",
   "./index.html",
